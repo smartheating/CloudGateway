cf1b5d07c6f36f790aec0b4617510dae0c4cc902
Stefan Basaric <st111465@stud.uni-stuttgart.de>
2019-06-11 16:12:31 +0200

    Enabled AWS Configuration

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,10 @@
 FROM maven:3-jdk-8
-RUN git clone https://github.com/smartheating/CloudGateway.git
-WORKDIR /CloudGateway/
+
+RUN git clone https://github.com/smartheating/CommonsModule.git
+WORKDIR /CommonsModule
+RUN mvn clean install -DskipTests -q
+RUN git clone https://github.com/smartheating/CloudGateway.git /CloudGateway
+WORKDIR /CloudGateway
 RUN mvn clean install -DskipTests -q
 ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-Dspring.profiles.active=aws","-jar","target/CloudGateway-0.0.1-SNAPSHOT.jar"]
 
--- src/main/java/de/smartheating/cloudgateway/CloudGatewayApplication.java
+++ src/main/java/de/smartheating/cloudgateway/CloudGatewayApplication.java
@@ -2,10 +2,14 @@ package de.smartheating.cloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
 @EnableZuulProxy
+@ComponentScan(basePackages = {"de.smartheating.SmartHeatingCommons","de.smartheating.cloudgateway"})
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
 public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
